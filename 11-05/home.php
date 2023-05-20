495c9d821dd974e6bfa6ac86e2969d3362fba0f5
esther <esthermariarosa@hotmail.com>
2023-05-19 22:24:30 -0400

    cards adicionados

--- 11-05/home.php
+++ 11-05/home.php
@@ -1,8 +1,48 @@
-<?php 
-    include("cabeca.php");
+<?php
+include("cabeca.php");
 ?>
 
+<style>
+    .cards-container-categoria {
+    display: flex;
+    justify-content: space-around;
+  }
+  
+  .card {
+    width: calc(33.33% - 10px);
+    /* O cálculo do width leva em consideração o espaçamento entre os cards */
+  }
+</style>
+<section class="cards-container-categoria">
+    <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="..." alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+    <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="..." alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+    <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="..." alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+</section>
 
-<?php 
-    include("rodape.php");
+<?php
+include("rodape.php");
 ?>
