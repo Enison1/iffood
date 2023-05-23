a9efa6ac472ea8b16d0256e28e2eda8a997e98c3
esther <esthermariarosa@hotmail.com>
2023-05-22 20:54:45 -0400

    alteração arquivo cabeça.php

--- 11-05/cabeca.php
+++ 11-05/cabeca.php
@@ -5,16 +5,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Aladin&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <link rel="stylesheet" href="style.css">
     <title>IF Food</title>
 </head>
 
 <body>
+    <script src="MDB5-STANDARD-UI-KIT-Free-6.3.1/src/js/free/dropdow.js"></script>
 
     <div class="cabecalho">
         <div class="menu">
@@ -40,21 +43,47 @@
                                 <a class="nav-link active" href="#">Carrinho</a>
                             </li>
                         </ul>
-                        <a class="nav-link active"  href="#"><img class="perfil-user" src="svg/Group 95.svg" alt=""></a>
+                        <!-- Avatar -->
+                        <div class="dropdown">
+                            <a class="dropdown-toggle d-flex align-items-center hidden-arrow" href="#"
+                                id="navbarDropdownMenuAvatar" role="button" data-mdb-toggle="dropdown"
+                                aria-expanded="false">
+                                <img src="svg/Group 95.svg" class="rounded-circle" height="35" alt="perfil"
+                                    loading="lazy" />
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuAvatar">
+                                <li>
+                                    <a class="dropdown-item" href="#">Meu perfil</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="#">Configurações</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="#">Sair</a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
+
                 </div>
             </nav>
         </div>
+    </div>
 
-        <div class="banner">
-            <div class="main">
-                <div>
-                    <p>BANNER</p>
+    <div class="banner">
+        <div class="main">
+            <div>
+                <p>Bem vindo ao IF Foods!</p>
+            </div>
+            <div class="input-group">
+                <div class="form-outline">
+                    <input width="300" placeholder="Pesquisar" type="search" id="form1" class="form-control" />
+                    <!-- <label class="form-label" for="form1">Pesquisar</label> -->
                 </div>
-                <div class="pesquisa">
-                    <input type="text" placeholder='PESQUISAR...'>
-                </div>
+                <button type="button" class="btn btn-warning">
+                    <i class="fas fa-search"></i>
+                </button>
             </div>
         </div>
-
+    </div>
     </div>
